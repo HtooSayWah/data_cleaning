758881a6fc5602768c780ba60692a5889bb4cb63
Aye Thu Zar <aye.thu.zar@fusionsol.com>
2020-03-17 08:47:30 +0630

    data cleaning first step

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
 import pandas as pd
 import converter
 from converter import uni512zg1,zg12uni51,test
+import re
 
 
 df = pd.read_csv("./data/sample_data.csv", usecols = ['Full Story'])
 print(df)
-text = df[1:2]
-print(zg12uni51(text.to_string()))
+# regExp = "[(+*$@!?/\,+-_=|''"";:။၊၁၂၃၄၅၆၇၈၉၀)a-zA-Z\s]"
+regExp = "[(+{}()*$@![]?/\,+-_=–|''"";:&^%#။၊၁၂၃၄၅၆၇၈၉၀“”‘)a-zA-Z\s]"
+for index,row in df.iterrows():
+    text = row['Full Story']
+    input = zg12uni51(text)
+    output = re.sub(regExp,'',input)
+    print(output)
 
 
